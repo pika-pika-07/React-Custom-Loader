c8d2d75a83db40c5d54ddd46024e11df0e4165ce
Pika-Pika-07 <parthbhatnagar40@gmail.com>
2023-11-07 13:16:04 +0530

    reduced loading time

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -16,7 +16,7 @@ const Body = () => {
       setTimeout(() => {
         console.log("Fetched");
         resolve([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]);
-      }, 300000);
+      }, 3000);
     });
   };
 
